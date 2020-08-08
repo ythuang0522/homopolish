c15d21f9efb9c3e45283a4f07366c69a2bc67b53
Spaert <asd850620@gmail.com>
2020-08-09 04:33:53 +0800

    Revert "feat: add training model"
    This reverts commit 8738f582c925575148438e10748284c7716028b2.

--- homopolish.py
+++ homopolish.py
@@ -1,7 +1,6 @@
 import argparse
 from version import __version__
 from modules.polish_interface import polish_genome
-from modules.train_interface import train_model
 
 def add_polish_arguments(parser):
     """
@@ -91,49 +90,11 @@ def add_polish_arguments(parser):
     )
 
     return parser
-def add_train_arguments(parser):
-    """
-    Add arguments to a parser for sub-command "polish"
-    :param parser: argeparse object
-    :return:
-    """
-    parser.add_argument(
-        "-d",
-        "--dataframe_dir",
-        type=str,
-        required=True,
-        help="[REQUIRED] Path to a directory for alignment dataframe."
-    )
-    parser.add_argument(
-        "-o",
-        "--output_dir",
-        type=str,
-        required=False,
-        default='./output/',
-        help="Path to the output directory. [output]"
-    )
-    parser.add_argument(
-        "-p",
-        "--output_prefix",
-        type=str,
-        required=False,
-        default='train',
-        help="Prefix for the train model. [train]"
-    )
-    parser.add_argument(
-        "-t",
-        "--threads",
-        type=int,
-        required=False,
-        default=1,
-        help="Number of threads to use. [1]"
-    )
-    return parser
 
 def main():
     parser = argparse.ArgumentParser(description="Homopolish is a SVM based polisher for polishing ONT-based assemblies. \n"
                                                  "1) polish: Run the polishing pipeline.\n"
-                                                 "2) train: Train your own SVM model.",
+                                                 "2) train: still in development",
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
         "-v",
@@ -146,16 +107,10 @@ def main():
     parser_polish = subparsers.add_parser('polish', help="Run the polishing pipeline.")
     add_polish_arguments(parser_polish)
 
-    parser_train = subparsers.add_parser('train', help="Train a model.")
-    add_train_arguments(parser_train)
-
     FLAGS, unparsed = parser.parse_known_args()
     if FLAGS.sub_command == 'polish':        
         polish_genome(FLAGS.mash_screen, FLAGS.assembly, FLAGS.model_path, FLAGS.sketch_path, FLAGS.genus, FLAGS.threads, \
                 FLAGS.output_dir, FLAGS.minimap_args, FLAGS.mash_threshold, FLAGS.download_contig_nums, FLAGS.debug)
-
-    elif FLAGS.sub_command == 'train':
-        train_model(FLAGS.dataframe_dir, FLAGS.output_dir, FLAGS.output_prefix, FLAGS.threads)
 
     elif FLAGS.version is True:
         print("Homopolish VERSION: ", __version__)
--- modules/prediction.py
+++ modules/prediction.py
@@ -5,14 +5,63 @@ import pandas as pd
 from joblib import Parallel, delayed
 from multiprocessing import Pool
 from sklearn.preprocessing import MinMaxScaler
-from modules import preprocessing as p
+
+def preprocessing(df):
+    new_draft = []
+    df['A'] = df['A'].astype(int) / df['coverage'].astype(int)
+    df['T'] = df['T'].astype(int) / df['coverage'].astype(int)
+    df['C'] = df['C'].astype(int) / df['coverage'].astype(int)
+    df['G'] = df['G'].astype(int) / df['coverage'].astype(int)
+    df['gap'] = df['gap'].astype(int) / df['coverage'].astype(int)
+    df['Ins_A'] = df['Ins_A'].astype(int) / df['coverage'].astype(int)
+    df['Ins_T'] = df['Ins_T'].astype(int) / df['coverage'].astype(int)
+    df['Ins_C'] = df['Ins_C'].astype(int) / df['coverage'].astype(int)
+    df['Ins_G'] = df['Ins_G'].astype(int) / df['coverage'].astype(int)    
+    df.loc[df['homopolymer'] > 10, 'homopolymer'] = 10 
+    for i in range(0,11):
+        if i not in df.homopolymer.value_counts():
+            name = 'homopolymer_' + str(i)
+            df[name] = 0
+    df = pd.DataFrame(df.drop(['draft'], axis=1))
+    df = pd.get_dummies(df, columns=['homopolymer'])
+    df = pd.DataFrame(df.drop(['position'], axis=1))
+    scaler = MinMaxScaler()
+    df['coverage'] = scaler.fit_transform(df[['coverage']])
+    return df
+
+def haplotype(df):
+    haplotype = np.zeros((df.shape[0],), dtype=int)
+    ins_index = df[df.draft == '-'].index.values
+    side = 10
+    dis = np.diff(ins_index)
+    idxs = np.where((dis < 10) & (dis > 1))[0]
+    
+
+    for i in idxs:        
+        head = df[df.index == ins_index[i]]
+        head_total = head[['Ins_A','Ins_T','Ins_C','Ins_G']].sum(axis=1).values[0]
+        tail = df[df.index == ins_index[i+1]]
+        tail_total = tail[['Ins_A','Ins_T','Ins_C','Ins_G']].sum(axis=1).values[0]
+        coverage = head.coverage.values[0]
+        if (head_total + tail_total) == coverage:   
+            if head_total > tail_total:
+                haplotype[ins_index[i]] = 1
+                haplotype[ins_index[i+1]] = 2
+            elif head_total < tail_total:
+                haplotype[ins_index[i]] = 2
+                haplotype[ins_index[i+1]] = 1
+            else:
+                haplotype[ins_index[i]] = 1
+                haplotype[ins_index[i+1]] = 1
+    df['haplotype'] = haplotype
+    return df
 
 def predict(dataframe, model, thread, path):    
 
     df = feather.read_dataframe(dataframe)
     result = df
-    df = p.haplotype(df)
-    df = p.preprocessing(df)
+    df = haplotype(df)
+    df = preprocessing(df)
 
     size = 1000
     list_of_X = [df.loc[i:i+size-1,:] for i in range(0, len(df), size)]
