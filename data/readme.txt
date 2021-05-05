cc1793c9623e2de436efd44a2ea9009ee1f5f21d
Yao-Ting Huang <ythuang@Yao-Tings-MacBook-Pro-2.local>
2021-05-05 22:40:25 +0800

    Add help message to main() when no arguments are provided. Reduce test data size by keeping on R9.4 genomes.

--- data/readme.txt
+++ data/readme.txt
@@ -2,5 +2,4 @@ Meta = Zymo Metagenomic data
 Isolate = bacterial isolate data
 Truth = truth genomes from Refseq references or hybrid Nanopore/Illumina assembly
 Draft = Medaka polished genomes
-Homopolish = Homopolished genomes
-e.g., Meta_Isolates_R9.4_homopolish.tar.gz includes the Homopolished genomes of metagenomic and isolate R9.4 datasets 
+e.g., Meta_Isolates_R9.4_draft.tar.gz includes the draf genomes of metagenomic and isolate R9.4 datasets 
--- homopolish.py
+++ homopolish.py
@@ -9,10 +9,10 @@ from os import path
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Homopolish is a SVM based polisher for polishing ONT-based assemblies. \n"
-                                                 "1) polish: Run the polishing pipeline.\n"
-                                                 "2) train: Train your own SVM model.\n"
-                                                "3) make_train_data: Make training data with reference genome.",
+    parser = argparse.ArgumentParser(description="Homopolish fixes systematic errors in ONT-based assemblies. \n",
+#                                                 "1) polish: Run the polishing pipeline.\n"
+#                                                 "2) train: Train your own SVM model.\n"
+#                                                "3) make_train_data: Make training data with reference genome.",
                                                      
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
@@ -23,17 +23,18 @@ def main():
         help="Show version."
     )
     subparsers = parser.add_subparsers(dest='sub_command')
-    parser_polish = subparsers.add_parser('polish', help="Run the polishing pipeline.")
+    parser_polish = subparsers.add_parser('polish', help="Polish your genomes.")
     add_polish_arguments(parser_polish)
     add_common_arguments(parser_polish)
-    parser_train = subparsers.add_parser('train', help="Train your own SVM model.")
+    parser_train = subparsers.add_parser('train', help="Train your model.")
     add_train_arguments(parser_train)
 
-    parser_make_train_data = subparsers.add_parser('make_train_data', help="Make training data with reference genome.") 
+    parser_make_train_data = subparsers.add_parser('make_train_data', help="Prepare training data with truth genome.") 
     add_train_data_arguments(parser_make_train_data)
     add_common_arguments(parser_make_train_data)
 
     FLAGS, unparsed = parser.parse_known_args()
+
     if FLAGS.sub_command == 'polish':
         this_directory = path.abspath(path.dirname(__file__))
         __pkg_path__ = os.path.join(this_directory,FLAGS.model_path)
@@ -50,6 +51,8 @@ def main():
     elif FLAGS.version is True:
         print("Homopolish VERSION: ", __version__)
 
+    else:
+        parser.print_help()
     
 if __name__ == "__main__":
     main()
